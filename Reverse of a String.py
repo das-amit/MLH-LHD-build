afd4dfac2431b6ba5f6e82c395e6647533605436
das-amit <91092039+das-amit@users.noreply.github.com>
2022-01-11 00:07:52 +0530

    Script for reversing srting
    This is a Python script to reverse a string

---
+++ Reverse of a String.py
@@ -0,0 +1,11 @@
+# Day 2: MLH Local Hack Day Challange
+
+# Write a Script to Reverse a String Reverse, reverse!
+# Find a way to take a string and turn it around backwards.
+
+
+#driver code
+
+char = str(input('Enter your string :'))
+char = char[::-1]
+print("The reversed string will be: ",char)
